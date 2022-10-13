d6cb2fb89aa8e232dc922ef9500c46fceaa34f67
Aakarsh Nair <aakarsh@pdx.edu>
2022-10-13 23:17:45 +0000

    WIP: Working on stream handler.

--- web_app/stream_handler.py
+++ web_app/stream_handler.py
@@ -18,6 +18,7 @@ MOCK = os.getenv('MOCK', 'false')
 
 define('mock', default=MOCK.lower() == 'true', help='run in a mocked mode')
 
+capture_only=True
 
 class StreamHandler(WebSocketHandler):
 
@@ -27,9 +28,10 @@ class StreamHandler(WebSocketHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.stream = PeriodicCallback(self.new_frame, 1000)
-        self.solver = Solver(trace_intermediate_images=True)
-        self.solver.startup()
+        self.stream = PeriodicCallback(self.new_frame, 5000)
+        if not capture_only:
+            self.solver = Solver(trace_intermediate_images=True)
+            self.solver.startup()
         self.img_num = 1
 
         Path(self.DATA_DIR).mkdir(parents=True, exist_ok=True)
@@ -105,11 +107,14 @@ class StreamHandler(WebSocketHandler):
             data = self.camera.capture()
             correlation_timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
-        try:
-            dec, ra, ori = self.solver.solve(data, trace_id=correlation_timestamp)
-        except Exception as exc:
-            error = str(exc)
-            logger.error(exc)
+        if not capture_only:
+            try:
+                dec, ra, ori = self.solver.solve(data, trace_id=correlation_timestamp)
+            except Exception as exc:
+                error = str(exc)
+                logger.error(exc)
+        else:
+            dec, rec, ori=[-1, -1, -1]
 
         # datetime.now().strftime('capture-%Y-%m-%d-%H-%M-%S.bmp')
         file_name = 'capture-'+correlation_timestamp+'.bmp'
@@ -121,11 +126,91 @@ class StreamHandler(WebSocketHandler):
         frame = base64.b64encode(img).decode('ascii')
         frame_message = 'data:image/jpg;base64, ' + frame
 
+
+    def inject_properties(img):
+        prucam_sysfs_ctx_settings = "/sys/devices/platform/prudev/context_settings/"
+        prucam_sysfs_ae_settings = "/sys/devices/platform/prudev/auto_exposure_settings/"
+
+        # possible camera context settings
+        ctx_settings = [
+            "analog_gain",
+            "blue_gain",
+            "coarse_time",
+            "context",
+            "digital_binning",
+            "fine_time",
+            "frame_len_lines",
+            "global_gain",
+            "green1_gain",
+            "green2_gain",
+            "red_gain",
+            "x_size",
+            "y_odd_inc",
+            "y_size",
+            ]
+
+        # auto exposure settings
+        ae_settings = [
+            "ae_ag_en",
+            "ae_ag_exposure_hi",
+            "ae_ag_exposure_lo",
+            "ae_damp_gain",
+            "ae_damp_max",
+            "ae_damp_offset",
+            "ae_dark_cur_thresh",
+            "ae_dg_en",
+            "ae_enable",
+            "ae_luma_target",
+            "ae_max_ev_step",
+            "ae_max_exposure",
+            "ae_min_ana_gain",
+            "ae_min_ev_step",
+            "ae_min_exposure",
+            "ae_roi_x_size",
+            "ae_roi_x_start_offset",
+            "ae_roi_y_size",
+            "ae_roi_y_start_offset",
+            ]
+
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        img_height, img_width, img_depth = img.shape
+        start_position=(10,10)
+        previous_position = start_position
+
+        time = datetime.now()
+        text_seperation=20
+
+        constant_properties = [('time', time), ('img_shape', img.shape)]
+        for (name, val) in constant_properties:
+            img = cv2.putText(img, f'{name}: {val}', previous_position, font, .4, (0, 255, 0), 1, cv2.LINE_AA)
+            previous_position = (previous_position[0], previous_position[1]+text_seperation)
+
+        for param in ctx_settings:
+            param_path = os.path.join(prucam_sysfs_ctx_settings, param)
+            with open(param_path, 'r+') as f:
+                val = str(f.read())
+                img = cv2.putText(img, f'{param}: {val}', previous_position, font, .4, (0, 255, 0), 1, cv2.LINE_AA)
+                previous_position = (previous_position[0], previous_position[1]+text_seperation)
+
+        for param in ae_settings:
+            param_path = os.path.join(prucam_sysfs_ae_settings, param)
+            with open(param_path, 'r+') as f:
+                val = str(f.read())
+                img = cv2.putText(img, f'{param}: {val}', previous_position, font, .4, (0, 255, 0), 1, cv2.LINE_AA)
+                previous_position = (previous_position[0], previous_position[1]+text_seperation)
+        return img
+
+
+
         def read_trace_data(trace_name):
+            if capture_only:
+                    return None
             file_name = f'solver-{trace_name}-{correlation_timestamp}.jpg'
             file_path = f'{self.DATA_DIR}/{file_name}'
             data = cv2.imread(file_path)
-            # resize image
+            # Resize image
             scale_percent = 80 # Percent of original size.
             width = int(data.shape[1] * scale_percent / 100)
             height = int(data.shape[0] * scale_percent / 100)
